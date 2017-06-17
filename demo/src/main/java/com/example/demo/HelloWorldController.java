8ba9d91ecc434ad8552dcb3277677f19cbf04e64
mayard <mayard@naver.com>
2017-06-17 14:24:57 +0900

    test

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping("/")
+//	@RequestMapping("/")
 	public String index(){
 		
 		return "hello world";
