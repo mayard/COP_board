8c35f99fad54b2ba93300b484d888be52dd13066
yjlee02 <yjlee02@daou-yjlee02>
2017-06-17 16:40:47 +0900

    1

--- demo/src/main/java/com/example/demo/service/BoardService.java
+++ demo/src/main/java/com/example/demo/service/BoardService.java
@@ -1,8 +1,28 @@
 package com.example.demo.service;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.dao.BoardDao;
+import com.example.demo.model.vo.BoardVo;
 
 @Service
 public class BoardService {
+
+	@Autowired
+	private BoardDao boardDao;
+	
+	public List<BoardVo> list() {
+		
+		return boardDao.selectList("board.board_select");
+	}
+	
+	public int insert_board(BoardVo boardvo) {
+		
+		return boardDao.insert("board.board_insert", boardvo);
+	}	
+	
 }
